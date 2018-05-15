a723f4a5980c563ec946d57579be974669fad77a
Lisa Cee <three_nineteen@yahoo.com>
2018-05-14 20:57:30 -0700

    server created with two get functions

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+const server = express();
 // add your server code starting here
+server.listen(5000, () => {
+    console.log('**Server running on port 5000**');
+});
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json({ posts })
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Problem retreiving data' })
+        })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const postId = req.params.id;
+
+    db.findById(postId)
+        .then(post => {
+            res.json({ post })
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Problem retreiving data' })
+        })
+})
